f013229d609c6fd946ab89120ab655507974887f
Luis Filipe Fialho <luisfilipefialho@proton.me>
2024-11-07 01:02:14 -0300

    fix

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -4,7 +4,7 @@ namespace DesafioPOO.Models
     {
         public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone");
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
         }
     }
 }
